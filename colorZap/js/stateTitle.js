df2bb11d678998eb67ce57ed53a48dbdbd8e2c16
jdiperi88 <jdiperi88@yahoo.com>
2018-10-30 13:40:50 -0400

    added a button that switches to main state file

--- colorZap/js/stateTitle.js
+++ colorZap/js/stateTitle.js
@@ -3,8 +3,14 @@ var StateTitle = {
 	preload: function() {
 		// title, path to image
 		game.load.image("logo", "images/title/gameLogo.png");
+
+		// ('ref_name', 'pathto.png', sprite_width, sprite_height, optional[number_of_cells])
+		game.load.spritesheet("buttons", "images/ui/buttons.png", 265, 75);
+
 		//(landscape, portrait)
-		game.scale.forceOrientation(false, true);
+		if (screen.width < 1500) {
+			game.scale.forceOrientation(false, true);
+		}
 	},
 
 	//setup objects, variables ,souds, text, good gusy, explosions
@@ -13,12 +19,31 @@ var StateTitle = {
 		//(x,y,library_key)
 		this.logo = game.add.sprite(game.world.centerX, 180, "logo");
 		this.logo.anchor.set(0.5, 0.5);
+
+		//START BUTTON
+		//game.add.button(x,y,imageKey,clickFunction,this,over_frame,normal_frame,down_frame)
+		this.btnStart = game.add.button(
+			game.world.centerX,
+			game.world.height - 200,
+			"buttons",
+			this.startGame,
+			this,
+			7,
+			6,
+			6
+		);
+		this.btnStart.anchor.set(0.5, 0.5);
 		this.setListeners();
+	},
+	startGame: function() {
+		game.state.start("StateMain");
 	},
 	setListeners: function() {
 		//(function,scope)
-		game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
-		game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+		if (screen.width < 1500) {
+			game.scale.enterIncorrectOrientation.add(this.wrongWay, this);
+			game.scale.leaveIncorrectOrientation.add(this.rightWay, this);
+		}
 	},
 	wrongWay: function() {
 		document.getElementById("wrongWay").style.display = "block";
